846968b15676ded12a55fec3c9950667ed035b40
ignacio14.castillor@gmail.com <ignacio.castillo-r@mail.escuelaing.edu.co>
2025-08-21 15:39:24 -0500

    feature: Preparacion y cambios

---
+++ Laboratorio-2-CVDS-DOSW-01/src/main/java/edu/dosw/lab/Laboratorio_2_CVDS_DOSW_01/Application.java
@@ -0,0 +1,7 @@
+package edu.dosw.lab;
+
+public class Application{
+    public static main(String[] args){
+        System.out.println("Proyecto Maven configurado y corriendo correctamente");
+    }
+}
